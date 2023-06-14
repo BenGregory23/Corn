b9ec81d7b9bff11166f327eab0b6cc875848d078
BenGregory23 <bengregorygbr@gmail.com>
2023-06-14 15:21:01 +0200

    moved theme

---
+++ theme/theme.ts
@@ -0,0 +1,14 @@
+
+export const lightTheme = {
+    background: 'white',
+    text: 'black',
+    button: '#34D1BF',
+    buttonTextColor: 'black',
+};
+
+export const darkTheme = {
+    background: 'black',
+    text: "white",
+    button: '#34D1BF',
+    buttonTextColor: 'black',
+}
