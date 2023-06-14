54cceabcafdc3da42d2b490c873f49e2ce3822cc
BenGregory23 <bengregorygbr@gmail.com>
2023-06-14 15:20:47 +0200

    Displaying list of friends

--- screens/FriendsScreen.tsx
+++ screens/FriendsScreen.tsx
@@ -1,12 +1,15 @@
 
-import React, { useState } from 'react'
-import {View, Text, SafeAreaView, TextInput, TouchableOpacity, Dimensions} from "react-native"
-import { FlatList } from 'react-native-gesture-handler'
+import React, { useEffect, useState } from 'react'
+import {View, Text, SafeAreaView, TextInput, TouchableOpacity, VirtualizedList} from "react-native"
+
 import { StyleSheet, TouchableHighlight, ScrollView } from 'react-native'
 import FriendCard from '../components/FriendCard'
 import "../assets/speaker.json"
 import { Plus } from 'lucide-react-native'
 import URL_BACKEND from '../constants/constants'
+import { useSelector } from 'react-redux'
+import { FlatList } from 'react-native-gesture-handler'
+
 
 
 
@@ -15,34 +18,62 @@ import URL_BACKEND from '../constants/constants'
 const FriendsScreen = ({navigation}) => {
     const [friendMail, setFriendMail] = useState("")
 
+    const [friends, setFriends] = useState([]);
+
+    // @ts-ignore
+    const user = useSelector(state => state.appReducer.user)
+
+
+    useEffect(() => {
+        fetch(URL_BACKEND + `/users/${user._id}/friends`,{
+            method: 'GET'
+        })
+        .then((response) => response.json()).then(response => {
+            // @ts-ignore
+            setFriends(response)
+        
+        })
+        
+    },[])
 
     const addFriend = () => {
-        fetch(URL_BACKEND + "/")
+        if(friendMail === ""){
+            console.log("Please select a friend")
+        }
+        
+        fetch(URL_BACKEND + `/users/${user._id}/friends`,{
+            method: 'POST',
+            body: friendMail    
+        }).then(response => {
+            console.log(response)
+        })
+        .catch(err => console.error(err))
 
     }
+
+    
+
+    const renderItem = ({ item }) => (
+        <FriendCard friend={ item} navigation={navigation}/>
+      );
 
     return(
         <View style={styles.container}>
           
           <View style={styles.search}>
-                <TextInput style={styles.searchInput} placeholder='Enter the mail of a friend' placeholderTextColor={"white"} />
-                <TouchableOpacity style={styles.searchButton}>
+                <TextInput style={styles.searchInput} placeholder='Enter the mail of a friend' placeholderTextColor={"white"} onChangeText={setFriendMail} />
+                <TouchableOpacity style={styles.searchButton} onPress={()=>addFriend()}>
                     <Plus width={30} fill='transparent' color='white'/> 
                 </TouchableOpacity>
             </View>
-    
-            <ScrollView style={styles.scrollView}>
-                <FriendCard navigation={navigation}/>   
-                <FriendCard navigation={navigation}/>      
-                <FriendCard navigation={navigation}/>   
-                <FriendCard navigation={navigation}/>
-                <FriendCard navigation={navigation}/>   
-                <FriendCard navigation={navigation}/>      
-                <FriendCard navigation={navigation}/>   
-                <FriendCard navigation={navigation}/>    
-            </ScrollView>
-          
-                 
+
+
+            <FlatList 
+            style={styles.scrollView}
+            data={friends}
+            renderItem={renderItem}
+            />
+        
         </View>
     )
 
@@ -62,7 +93,7 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         borderColor: "#34D1BF",
         borderWidth: 2,
-        borderRadius: 100,
+        borderRadius: 12,
         paddingLeft: 20,
         paddingRight: 5,
         paddingVertical: 5,
@@ -72,11 +103,11 @@ const styles = StyleSheet.create({
     },
     searchInput:{
         flex: 1,
-     
+        color: "white",
      height: "100%",
     },
     searchButton:{
-        borderRadius: 50,
+        borderRadius: 10,
         justifyContent: 'center',
         alignItems: 'center',
         paddingVertical: 10,
