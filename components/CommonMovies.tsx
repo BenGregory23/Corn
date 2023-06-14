13778dce8f0f0225b3d130d900ebf069b3637638
BenGregory23 <bengregorygbr@gmail.com>
2023-06-14 15:19:40 +0200

    removed default header and added a custom one

--- components/CommonMovies.tsx
+++ components/CommonMovies.tsx
@@ -1,12 +1,42 @@
-import React from "react"
-import {View, Text, StyleSheet} from "react-native"
+import React, { useEffect, useState } from "react"
+import {View, Text, StyleSheet, TouchableOpacity} from "react-native"
+import CustomModal from "./CustomModal"
+import URL_BACKEND from "../constants/constants";
+import { ChevronLeft } from 'lucide-react-native';
+import { useNavigation } from "@react-navigation/native";
 
 
-const CommonMovies = ({name}) =>{
+
+const CommonMovies = ({id}) =>{
+
+    const navigation = useNavigation();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    }
+
+    useEffect(()=>{
+        console.log(id)
+        fetch(URL_BACKEND + `/users/${4}`)
+
+    },[])
 
     return (
         <View style={styles.container}>
-            <Text>Common Movies</Text>
+            <View style={styles.header}>
+              
+            <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate("Friends")}>
+                <ChevronLeft size={30} color="white" />
+                
+            </TouchableOpacity>
+            <Text style={styles.title}>Shared Movies</Text>
+            </View>
+            
+
+            <CustomModal visible={isModalOpen} onClose={closeModal} title={""}>
+
+            </CustomModal>
         </View>
     )
 }
@@ -16,10 +46,27 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "black",
-        display: "flex",
-        justifyContent: "center",   
-        alignItems:"center"
-    }
+       
+    },
+    header:{
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        flexDirection: "row",
+        margin: 20,
+    },
+    title:{
+        color:'white',
+        fontSize: 29,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 0,
+        
+    }, 
+    backButton:{
+     
+        marginRight: 10,
+         zIndex: 100,
+     },
 })
 
 export default CommonMovies;
