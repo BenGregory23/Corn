8edd77c5cb98e2ea724b6d68121244419336cdb3
BenGregory23 <bengregorygbr@gmail.com>
2023-09-02 18:30:29 +0200

    added constant

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,4 +1,5 @@
 
-const URL_BACKEND = 'https://evening-shore-83627.herokuapp.com';
+export const OLD_URL_BACKEND = 'https://evening-shore-83627.herokuapp.com';
+export const URL_BACKEND = 'https://corn-backend-10bb4fab433c.herokuapp.com/api';
 
-export default URL_BACKEND;
+
