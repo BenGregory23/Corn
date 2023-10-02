3edcaa4dc5a9f49392df8859e3509adc726ccd0c
BenGregory23 <bengregorygbr@gmail.com>
2023-10-02 14:10:23 +0200

    added text and started to organise the file by screen

--- lang/lang.ts
+++ lang/lang.ts
@@ -1,12 +1,12 @@
 export const EN = {
     watchlist: "WATCHLIST",
     friends: "My friends",
+    groups: "My groups",
     logout: "Log out",
     addFriends: "Add friends!",
     addFriendsText: "Add friends by entering their email address. You can then see what movies you both want to watch.",
     friendAdded: "Friend added!",
     add: "Add",
-    settings: "Settings",
     sharedMovies: "Shared Movies",
     displaySharedMovies: "Display shared movies",
     loadingSentences: [
@@ -21,21 +21,24 @@ export const EN = {
         "Matching your preferences...",
         "Searching for movie gems...",
     ],
-    // Settings content
-    movieProviders: "Movie providers",
-    movieProvidersText: "You can select the movie providers you have access to. This way, the movies we will recommend will be available on these platforms.",
-    addMovies: "Add some movies!"
+    settingsPage: {
+        userInfo: "Personal information",
+        settings: "Settings",
+        movieProviders: "Movie providers",
+        movieProvidersText: "You can select the movie providers you have access to. This way, the movies we will recommend will be available on these platforms.",
+        addMovies: "Add some movies!"
+    }
 };
 
 export const FR = {
     watchlist: "MES FILMS",
     friends: "Mes amis",
+    groups: "Mes groupes",
     logout: "Déconnexion",
-    addFriends: "Ajoute tes amis !",
-    addFriendsText: "Ajoute tes amis en entrant leurs email. Vous pourrez ensuite voir quels films vous avez en commun.",
+    addFriends: "Ajoutes tes amis !",
+    addFriendsText: "Ajoutes tes amis en entrant leurs email. Vous pourrez ensuite voir quels films vous avez en commun.",
     friendAdded: "Amis ajouté !",
     add: "Ajouter",
-    settings: "Paramètres",
     sharedMovies: "Films en commun",
     displaySharedMovies: "Films en commun",
     loadingSentences: [
@@ -50,8 +53,11 @@ export const FR = {
         "Chargement de vos préférences...",
         "À la recherche de pépite...",
     ],
-      // Settings content
-      movieProviders: "Fournisseurs de contenu",
-      movieProvidersText: "Vous pouvez sélectionner les fournisseurs de contenu auxquels vous avez accès. Ainsi, les films que nous vous proposerons seront disponibles sur ces plateformes.",
-      addMovies: "Ajouter des films !"
+    settingsPage: {
+        userInfo: "Informations personnelles",
+        settings: "Paramètres",
+        movieProviders: "Fournisseurs de contenu",
+        movieProvidersText: "Vous pouvez sélectionner les fournisseurs de contenu auxquels vous avez accès. Ainsi, les films que nous vous proposerons seront disponibles sur ces plateformes.",
+        addMovies: "Ajouter des films !"
+    }
 }
