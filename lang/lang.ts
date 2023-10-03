e904c1415375c9e7f36cc36e6d7276d1d531e64b
BenGregory23 <bengregorygbr@gmail.com>
2023-10-03 10:41:50 +0200

    added fields

--- lang/lang.ts
+++ lang/lang.ts
@@ -7,8 +7,6 @@ export const EN = {
     addFriendsText: "Add friends by entering their email address. You can then see what movies you both want to watch.",
     friendAdded: "Friend added!",
     add: "Add",
-    sharedMovies: "Shared Movies",
-    displaySharedMovies: "Display shared movies",
     loadingSentences: [
         "Loading recommendations...",
         "Finding movies for you...",
@@ -27,6 +25,11 @@ export const EN = {
         movieProviders: "Movie providers",
         movieProvidersText: "You can select the movie providers you have access to. This way, the movies we will recommend will be available on these platforms.",
         addMovies: "Add some movies!"
+    },
+    sharedMoviesPage: {
+        sharedMovies: "Shared Movies",
+        displaySharedMovies: "Display shared movies",
+        help: "Choose if you want to see the movies that both you and your friend want to see or all the movies your friend has added to his list"
     }
 };
 
@@ -39,8 +42,7 @@ export const FR = {
     addFriendsText: "Ajoutes tes amis en entrant leurs email. Vous pourrez ensuite voir quels films vous avez en commun.",
     friendAdded: "Amis ajouté !",
     add: "Ajouter",
-    sharedMovies: "Films en commun",
-    displaySharedMovies: "Films en commun",
+  
     loadingSentences: [
         "Chargement des recommandations...",
         "Recherche de films...",
@@ -59,5 +61,10 @@ export const FR = {
         movieProviders: "Fournisseurs de contenu",
         movieProvidersText: "Vous pouvez sélectionner les fournisseurs de contenu auxquels vous avez accès. Ainsi, les films que nous vous proposerons seront disponibles sur ces plateformes.",
         addMovies: "Ajouter des films !"
+    },
+    sharedMoviesPage: {
+        sharedMovies: "Films en commun",
+        displaySharedMovies: "Afficher les films en commun",
+        help: "Choisissez si vous voulez voir les films en commun avec votre ami ou alors tous les films que votre ami a ajouté à sa liste."
     }
 }
